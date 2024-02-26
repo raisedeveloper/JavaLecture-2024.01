7d0a88894b65d6bc866c7b760b396c11889d2a43
raisedeveloper <ilove4903@naver.com>
2024-02-27 01:52:27 +0900

    bbs

---
+++ src/hello/Hello.java
@@ -0,0 +1,10 @@
+package hello;
+
+import java.time.LocalDateTime;
+
+public class Hello {
+	private int id;
+	private String comment;
+	private String userName;
+	private LocalDateTime localTime;
+}
